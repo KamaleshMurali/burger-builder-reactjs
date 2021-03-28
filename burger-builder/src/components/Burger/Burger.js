687e2f902770d1f58732bab28736d9a7fc1bc833
KamaleshMurali <kamalmurali15@gmail.com>
2021-03-28 22:32:57 +0530

    Burger has been created using state values hard-coded in the code

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,16 +3,19 @@ import BurgerIngredients from './BurgerIngredient/BurgerIngredient';
 import classes from '../Burger/Burger.module.css'
 
 const burger = ( props ) => {
-    return (
-      <div className={classes.Burger}>
-        <BurgerIngredients type="bread-top"></BurgerIngredients>
-        <BurgerIngredients type="cheese"></BurgerIngredients>
-        <BurgerIngredients type="bacon"></BurgerIngredients>
-        <BurgerIngredients type="meat"></BurgerIngredients>
-        <BurgerIngredients type="salad"></BurgerIngredients>
-        <BurgerIngredients type="bread-bottom"></BurgerIngredients>
-      </div>
-    );
+
+  let modifiedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, key) => {
+      return <BurgerIngredients key={key + igKey} type={igKey}></BurgerIngredients>
+    })
+  })
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top"></BurgerIngredients>
+      {modifiedIngredients}
+      <BurgerIngredients type="bread-bottom"></BurgerIngredients>
+    </div>
+  );
 }
 
 export default burger;
