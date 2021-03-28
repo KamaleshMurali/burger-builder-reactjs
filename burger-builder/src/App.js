402213988cbe7db3929c73adf445f700c8334b37
KamaleshMurali <kamalmurali15@gmail.com>
2021-03-28 18:13:51 +0530

    1. Created based component consist of layout and main container 2. Created Higher Order Component for Wrapping Adjacent JSX

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './components/Layout';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>Test</Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
